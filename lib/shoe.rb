c9124e0f25b5cb91c4a1de976d58d71296ae9d68
Brigette LeVert <bnl.levert@gmail.com>
2020-01-19 21:36:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe    # Make your shoe class here!
 
-attr_accessor :brand, :color, :size, :material
+attr_accessor :brand, :color, :size, :material, :condition
 
 def initialize(brand)
   @brand = brand
